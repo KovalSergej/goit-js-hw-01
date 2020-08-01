25fb4022700fe4b7ac1041a16f94fb72975f7447
KovalSergej <koval.sergej.2014@gmail.com>
2020-08-01 19:42:49 +0300

    01 Malets js - second commit task-06 change (booleans ne sravnivat' mejdu soboj)

--- task-06.js
+++ task-06.js
@@ -3,17 +3,15 @@
 let input = 0;
 let total = 0;
 
-
 do {
   input = prompt("Укажите число");
   if (input === null) {
     break;
   }
-  if (Number.isNaN(+input) === true) {
+  if (Number.isNaN(+input)) {
     alert(`Было введено не число, попробуйте еще раз`);
     continue;
   }
   total = +input + total;
-
 } while (input !== null);
 alert(`Общая сумма чисел равна ${total}`);
