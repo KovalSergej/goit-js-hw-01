8b6ffbad86737dd6d16450cdb9f9823bb4a2839f
KovalSergej <koval.sergej.2014@gmail.com>
2020-08-01 19:38:27 +0300

    01 Malets js - second commit task-06 change (unarnyj operator dobavlen)

--- task-06.js
+++ task-06.js
@@ -3,18 +3,17 @@
 let input = 0;
 let total = 0;
 
-let inputToNumber;
 
 do {
   input = prompt("Укажите число");
   if (input === null) {
     break;
   }
-  inputToNumber = Number(input);
-  if (Number.isNaN(inputToNumber) === true) {
+  if (Number.isNaN(+input) === true) {
     alert(`Было введено не число, попробуйте еще раз`);
     continue;
   }
-  total = inputToNumber + total;
+  total = +input + total;
+
 } while (input !== null);
 alert(`Общая сумма чисел равна ${total}`);
